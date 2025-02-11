41f9ece80a21e48c1e7187beedde8d89d788b622
Oussama <Messamessus@yahoo.com>
2025-02-11 22:50:26 +0100

    deployment preparation

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
 import Recommened from './Recommened'
 import News from './News'
+import axios from 'axios';  // Default import - Correct way
 
 const Home = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/product`)
+      .then((response) => {
+        setProduct(response.data.deta); // Potential typo: 'deta' should likely be 'data'
+      })
+      .catch((error) => {
+        console.log(error); // Consider showing a user-friendly error message
+      });
+  }, []);
+
+  const latestProducts = product.slice(0, 3);
+
   return (
     <>
         <Banner/>
@@ -16,3 +31,5 @@ const Home = () => {
 }
 
 export default Home
+
+
